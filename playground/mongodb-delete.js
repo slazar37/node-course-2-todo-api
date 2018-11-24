89d4ceb730866feda17c615e37efe78038654557
root <root@sample>
2018-11-24 18:31:10 -0500

    Add delete script

---
+++ playground/mongodb-delete.js
@@ -0,0 +1,44 @@
+// const MongoClient = require('mongodb').MongoClient;
+const { MongoClient, ObjectID } = require('mongodb');
+
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+    if (err) {
+        return console.log('Unable to connect to MongoDB server');
+    }
+    console.log('Connected to MongoDB server');
+
+    // deleteMany
+    /*
+    db.collection('Todos').deleteMany({ text: 'Eat lunch' }).then((result) => {
+        console.log(result);
+    });
+    */
+
+    // deleteOne
+    /*
+    db.collection('Todos').deleteOne({ text: 'Eat lunch' }).then((result) => {
+        console.log(result);
+    });
+    */
+
+    // findOneAndDelete
+    /*
+    db.collection('Todos').findOneAndDelete({ completed: false }).then((result) => {
+        console.log(result);
+    });
+    */
+
+    // ObjectId("5bf709d354d70c11d4138926")
+
+    // remove duplicate users
+    db.collection('Users').deleteMany({ name: 'Andrew' });
+
+    db.collection('Users').findOneAndDelete({ 
+        _id: new ObjectID("5bf709d354d70c11d4138926") 
+    }).then((results) => {
+        console.log(JSON.stringify(results, undefined, 2));
+    });
+
+    // db.close();
+});
