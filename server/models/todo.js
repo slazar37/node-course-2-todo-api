968e1ebd18b799895aa32e5b417d48bf2e456f70
root <root@sample>
2019-01-07 21:37:13 -0500

    Add POST /todos route and refactor mongoose

---
+++ server/models/todo.js
@@ -0,0 +1,21 @@
+var mongoose = require('mongoose');
+
+// models are used to indicate to mongoose how to store data:
+var Todo = mongoose.model('Todo', {
+    text: {
+        type: String,
+        required: true,
+        minlength: 1,
+        trim: true
+    },
+    completed: {
+        type: Boolean,
+        default: false
+    },
+    completedAt: {
+        type: Number,
+        default: null
+    }
+});
+
+module.exports = { Todo };
