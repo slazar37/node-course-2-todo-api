9b7d9f67548d1480cf543d2d0353e57419603239
root <root@sample>
2019-01-09 10:41:40 -0500

    Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,11 +1,13 @@
 // library imports
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 // local imports
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+
 
 var app = express();
 
@@ -24,6 +26,27 @@ app.post('/todos', (req, res) => {
     });
 });
 
+
+app.get('/todos/:id', (req, res) => {
+    var id = req.params.id;
+
+    if (!(ObjectID.isValid(id))) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id)
+        .then((todo) => {
+            if (!todo) {
+                return res.status(400).send();
+            } else {
+                res.send({ todo });
+            }
+        })
+        .catch((error) => {
+            res.status(400).send();
+        });
+});
+
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({ 
@@ -33,6 +56,7 @@ app.get('/todos', (req, res) => {
         res.status(400).send(e);
     })
 });
+
 
 app.listen(3000, () => {
     console.log('Started on port 3000');
